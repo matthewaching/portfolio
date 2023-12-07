e3773c23e19f61dee7aa2f58064fdcd324323172
unknown <matthewaching@gmail.com>
2023-12-06 21:24:40 -0600

    Add baseline layout for page

---
+++ src/app/MainNav/MainNav.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const MainNav = () => {
+    return (
+        <div className="flex flex-col self-center justify-self-end gap-8">
+            <span>
+                Home
+            </span>
+            <span>
+                Blog
+            </span>
+            <span>
+                Projects
+            </span>
+            <span>
+                Contact
+            </span>
+        </div>
+    );
+};
+
+export default MainNav;
