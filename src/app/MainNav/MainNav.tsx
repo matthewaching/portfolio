8f8c28f5480529ee87f388a239a60b1a9b2b4640
unknown <matthewaching@gmail.com>
2024-11-06 20:26:41 -0600

    Commit leftover changes

--- src/app/MainNav/MainNav.tsx
+++ src/app/MainNav/MainNav.tsx
@@ -1,21 +1,42 @@
 import Link from 'next/link';
 import React from 'react'
 
+type PageRoute = {
+    id: string,
+    label: string,
+};
+
+const routes: PageRoute[] = [
+    {
+        id: '',
+        label: 'Home',
+    },
+    {
+        id: 'blog',
+        label: 'Blog',
+    },
+    {
+        id: 'projects',
+        label: 'Projects',
+    },
+    {
+        id: 'contact',
+        label: 'Contact',
+    },
+];
+
 const MainNav = () => {
     return (
-        <div className="flex flex-col self-center justify-self-end gap-8">
-            <Link href='/'>
-                Home
-            </Link>
-            <Link href='/blog'>
-                Blog
-            </Link>
-            <span>
-                Projects
-            </span>
-            <span>
-                Contact
-            </span>
+        <div className="flex flex-col self-center justify-self-end gap-16">
+            {routes.map(route => (
+                <Link
+                    key={route.id}
+                    href={`/${route.id}`}
+                    className="text-xl"
+                >
+                    {route.label}
+                </Link>
+            ))}
         </div>
     );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 const Home = () => {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center p-24">
-            This is the homepage
+        <main className="flex text-5xl min-h-screen flex-col items-center justify-center p-24">
+            Hi, I'm Matt!
         </main>
     );
 };
