5e958e1000ba9eb65951c77fc4e48644d6c23e8d
unknown <matthewaching@gmail.com>
2024-11-06 20:27:20 -0600

    Fix apostrophe error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const Home = () => {
     return (
         <main className="flex text-5xl min-h-screen flex-col items-center justify-center p-24">
-            Hi, I'm Matt!
+            Hi, I&apos;m Matt!
         </main>
     );
 };
