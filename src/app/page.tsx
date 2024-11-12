befe3fa088b6273822eee4230fe239aa441a1e56
unknown <matthewaching@gmail.com>
2024-11-12 09:56:00 -0600

    Add initial reflection div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,11 @@ const Home = () => {
     return (
         <main className="flex text-5xl min-h-screen flex-col items-center justify-center">
             <div id='landing-frame' className='h-dvh w-dvw flex flex-col items-center justify-center'>
-                <span>{'Hi, I\'m Matt!'}</span>
+                <span className='pb-2'>{'Hi, I\'m Matt!'}</span>
+                <div className='relative pb-2 scale-y-[-1]' id='reflection'>
+                    <span>{'Hi, I\'m Matt!'}</span>
+                    <div className='fixed top-0 left-0 w-full h-full bg-gradient-to-b via-inherit to-[rgba(0,0,0,0)]' />
+                </div>
                 <span className='mt-64'>WIP - Scroll down for now</span>
             </div>
             <MainNav />
